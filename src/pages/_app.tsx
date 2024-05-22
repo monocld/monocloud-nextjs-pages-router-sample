39ef71274d1d32286082e27a077ae36185d2d285
Viraj Shukla <virajshukla00@gmail.com>
2024-05-22 15:35:01 +0530

    Add the AuthProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { MonoCloudAuthProvider } from "@monocloud/nextjs-auth/client";
+import Header from "@/components/header";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({
+  Component,
+    pageProps: { session, ...pageProps }
+}: AppProps) {
+  return (
+    <MonoCloudAuthProvider>
+      <Header />
+      <Component {...pageProps} />
+    </MonoCloudAuthProvider>
+  )
 }
